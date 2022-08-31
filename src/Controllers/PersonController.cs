19255d989a76abc09e698630e30948ddd30253bc
Kleiton MQ <kleiton.x5@gmail.com>
2022-08-31 14:05:45 -0300

    Inclusão utilização dos verbos http, post, put, delete, get.

--- src/Controllers/PersonController.cs
+++ src/Controllers/PersonController.cs
@@ -11,10 +11,27 @@ namespace src.Controllers;
 [Route("[controller]")] 
 public class PersonController : ControllerBase{
 //antes de qualquer método precisa dizer que tipo de requisição http ele vai usar.
-[HttpGet]
+[HttpGet] //Selecionar dados
 public Pessoa Get(){
     Pessoa pessoa = new Pessoa("eu", 15, "654321");
-
+    Contrato novoContrato = new Contrato("abc123", 20.13);
+    pessoa.contratos.Add(novoContrato);
     return pessoa;
+}
+
+[HttpPost] //Enviar dados
+public Pessoa Post([FromBody]Pessoa pessoa){
+    
+    return pessoa;
+}
+
+[HttpPut("{id}")] //Atualizar informações
+public string Update([FromRoute]int id){
+    return "Dados do ID " + id +" atualizados.";
+}
+
+[HttpDelete("{id}")] //Deletar dados.
+public string Delete([FromRoute]int id){
+    return "deleteado pessoa de ID "+ id;
 }
 }
--- src/models/Contrato.cs
+++ src/models/Contrato.cs
@@ -1,5 +1,23 @@
 namespace src.Models;
 
 public class Contrato{
-    
+
+public Contrato(){
+    this.dataCriacao = DateTime.Now;
+    this.valor = 0;
+    this.tokenId = "00000";
+    this.pago = false;
+}
+
+public Contrato(string tokenId, double valor){
+    this.dataCriacao = DateTime.Now;
+    this.tokenId = tokenId;
+    this.valor = valor;
+    this.pago = false;
+}
+    public DateTime dataCriacao { get; set; }
+    public string tokenId { get; set; }
+    public double valor { get; set; }
+
+    public bool pago { get; set; }
 }
